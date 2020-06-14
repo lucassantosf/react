68d7d038c69e9ef3ee9b7cc19f0c64ce2946780f
lucassantosf <lucas46em@gmail.com>
2020-06-14 13:58:08 -0300

    studying react

---
+++ src/pages/Main/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container, Form, SubmitButton } from "./styles";
+import { FaGithubAlt, FaPlus } from "react-icons/fa";
+
+function Main() {
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+        <SubmitButton disabled>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
+}
+
+export default Main;
