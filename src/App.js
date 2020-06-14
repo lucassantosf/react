1709f27aaa2498c2d922f99e9c0edd1cbded8b15
lucassantosf <lucas46em@gmail.com>
2020-06-13 21:55:45 -0300

    Starting  a new react app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React, {Component} from 'react'; 
-import Routes from "./routes";
- 
-import './styles.css';
+import React from "react";
 
-import Header from './components/Header';
-import Main from './pages/main';
-
-const App = () => (
-  <div className="App">
-    <Header />
-    <Routes /> 
-  </div>
-);
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
 
 export default App;
--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-	baseURL:'https://free-nba.p.rapidapi.com' 
-});
-
-export default api;
