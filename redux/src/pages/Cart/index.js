c595f5cb51ebbfac58e4e79502e81b5ceed961f9
lucassantosf <lucas46em@gmail.com>
2020-07-13 19:02:19 -0300

    styling header and cart page

--- redux/src/pages/Cart/index.js
+++ redux/src/pages/Cart/index.js
@@ -1,5 +1,66 @@
 import React from "react";
+import { Container, ProductTable, Total } from "./styles";
+
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from "react-icons/md";
 
 export default function Cart() {
-  return <h1>Carrinho</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Produto</th>
+            <th>Quantidade</th>
+            <th>Subtotal</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-everlast-kromus-masculino/26/AXB-1745-026/AXB-1745-026_zoom2.jpg?ts=1567596394&ims=326x"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tenis muito dahora</strong>
+              <span>122</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>$258.00</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar Pedido</button>
+        <Total>
+          <span>Total</span>
+          <strong>R$199</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
--- redux/src/pages/Home/index.js
+++ redux/src/pages/Home/index.js
@@ -1,5 +1,99 @@
 import React from "react";
+import { ProductList } from "./styles";
+import { MdAddShoppingCart } from "react-icons/md";
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-kromus-masculino/26/AXB-1745-026/AXB-1745-026_zoom2.jpg?ts=1567596394&ims=326x"
+          alt="Tênis"
+        ></img>
+        <strong>Tênis muito legal</strong>
+        <span>R$129.99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-kromus-masculino/26/AXB-1745-026/AXB-1745-026_zoom2.jpg?ts=1567596394&ims=326x"
+          alt="Tênis"
+        ></img>
+        <strong>Tênis muito legal</strong>
+        <span>R$129.99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-kromus-masculino/26/AXB-1745-026/AXB-1745-026_zoom2.jpg?ts=1567596394&ims=326x"
+          alt="Tênis"
+        ></img>
+        <strong>Tênis muito legal</strong>
+        <span>R$129.99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-kromus-masculino/26/AXB-1745-026/AXB-1745-026_zoom2.jpg?ts=1567596394&ims=326x"
+          alt="Tênis"
+        ></img>
+        <strong>Tênis muito legal</strong>
+        <span>R$129.99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-kromus-masculino/26/AXB-1745-026/AXB-1745-026_zoom2.jpg?ts=1567596394&ims=326x"
+          alt="Tênis"
+        ></img>
+        <strong>Tênis muito legal</strong>
+        <span>R$129.99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-everlast-kromus-masculino/26/AXB-1745-026/AXB-1745-026_zoom2.jpg?ts=1567596394&ims=326x"
+          alt="Tênis"
+        ></img>
+        <strong>Tênis muito legal</strong>
+        <span>R$129.99</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
