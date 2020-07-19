da5735cc07c690d34d0b460a976646dd4d884499
lucassantosf <lucas46em@gmail.com>
2020-07-19 11:22:32 -0300

    Finishing project, integration redux+saga and checking product's stock before add one to cart

--- redux-example-project/src/pages/Home/index.js
+++ redux-example-project/src/pages/Home/index.js
@@ -20,10 +20,10 @@ class Home extends Component {
     this.setState({ products: data });
   }
 
-  handleAddProduct = (product) => {
-    const { addToCart } = this.props;
+  handleAddProduct = (id) => {
+    const { addToCartRequest } = this.props;
 
-    addToCart(product);
+    addToCartRequest(id);
   };
 
   render() {
@@ -38,7 +38,7 @@ class Home extends Component {
             <span>{product.priceFormatted}</span>
             <button
               type="button"
-              onClick={() => this.handleAddProduct(product)}
+              onClick={() => this.handleAddProduct(product.id)}
             >
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" />
