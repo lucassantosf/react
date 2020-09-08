35e61923067292bed78d8fa32561560aa975e4b5
lucassantosf <lucas46em@gmail.com>
2020-09-08 18:58:38 -0300

    Storing users, and auth features

---
+++ adonis-api/app/Controllers/Http/ForgotPasswordController.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const crypto = require("crypto");
+const User = use("App/Models/User");
+const Mail = use("Mail");
+
+class ForgotPasswordController {
+  async store({ request, response }) {
+    try {
+      const email = request.input("email");
+      const user = await User.findByOrFail("email", email);
+
+      user.token = crypto.randomBytes(10).toString("hex");
+      user.token_created_at = new Date();
+
+      await user.save();
+
+      await Mail.send(
+        ["emails.forgot_password"],
+        {
+          email,
+          token: user.token,
+          link: `${request.input("redirect_url")}?token=${user.token}`,
+        },
+        (message) => {
+          message
+            .to(user.email)
+            .from("lucas@gmail.com", "Lucas Santos")
+            .subject("Recuper senha");
+        }
+      );
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: "Algo nao deu certo, este email existe?" } });
+    }
+  }
+}
+
+module.exports = ForgotPasswordController;
