c21dddc81aba332d7edd43da93bbe6205087e4e9
lucassantosf <lucas46em@gmail.com>
2020-09-30 18:03:35 -0300

    Signin layout

--- conceitos/src/pages/Auth/SignIn/index.js
+++ conceitos/src/pages/Auth/SignIn/index.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-//import {Container} from 'styles';
+import { Container, SignForm } from "../styles";
 
-const SignIn = () => <div></div>;
+import Button from "../../../styles/components/Button";
+
+const SignIn = () => (
+  <Container>
+    <SignForm onSubmit={() => {}}>
+      <h1>Boas Vindas</h1>
+
+      <span>Email</span>
+      <input type="email" name="email" value="" />
+
+      <span>Senha</span>
+      <input type="password" name="password" value="" />
+
+      <Button size="big" type="submit">
+        Entrar
+      </Button>
+    </SignForm>
+  </Container>
+);
 
 export default SignIn;
