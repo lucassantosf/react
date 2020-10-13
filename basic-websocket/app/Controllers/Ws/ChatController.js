c5a3e61f1edc72e91d54470d2c6d053005103515
lucassantosf <lucas46em@gmail.com>
2020-10-13 12:47:03 -0300

    Studyng websocket

---
+++ basic-websocket/app/Controllers/Ws/ChatController.js
@@ -0,0 +1,13 @@
+'use strict'
+
+class ChatController {
+  constructor ({ socket, request }) {
+    this.socket = socket
+    this.request = request
+  }
+  onMessage(data){
+    this.socket.broadcastToAll('message',data)
+  }
+}
+
+module.exports = ChatController
