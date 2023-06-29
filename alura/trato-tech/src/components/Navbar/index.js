4415bc3e908f59cffea74cf07a7a059bd3ff146e
Lucas <lucas46em@gmail.com>
2023-06-29 10:34:25 -0300

    Cart

--- alura/trato-tech/src/components/Navbar/index.js
+++ alura/trato-tech/src/components/Navbar/index.js
@@ -3,6 +3,7 @@ import { ReactComponent as Logo } from '../../assets/logo.svg'
 import classNames from 'classnames'
 import {RiShoppingCart2Line,RiShoppingCartFill} from 'react-icons/ri'
 import Busca from 'components/Busca'
+import { Link, useLocation, useNavigate} from 'react-router-dom'
 
 const iconeProps = {
     color:'white',
@@ -10,21 +11,24 @@ const iconeProps = {
 }
 
 export default function Navbar(){
+    const location = useLocation()
+    const navigate = useNavigate()
+
     return (
         <nav className={styles.nav}>
-            <Logo className={styles.logo} />
+            <Logo className={styles.logo} onClick={()=>navigate('/')} />
             <div className={styles.links}>
                 <div>
-                    <a href="/" className={classNames(styles.link,{[styles.selected]:window.location.pathname==='/'})}> Pagina Inicial</a>
+                    <Link to="/" className={classNames(styles.link,{[styles.selected]:location.pathname==='/'})}> Pagina Inicial</Link>
                 </div>
             </div>
             <div className={styles.busca}>
                 <Busca />
             </div>
-            <div className={styles.busca}>
-                <a href='/carrinho'>
-                    {window.location.pathname === '/carrinho' ? <RiShoppingCartFill {...iconeProps} /> : <RiShoppingCart2Line {...iconeProps} /> }
-                </a>
+            <div className={styles.icones}>
+                <Link to='/carrinho'>
+                    {location.pathname === '/carrinho' ? <RiShoppingCartFill {...iconeProps} /> : <RiShoppingCart2Line {...iconeProps} /> }
+                </Link>
             </div>
         </nav>
     )
--- alura/trato-tech/src/store/index.js
+++ alura/trato-tech/src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import categoriaSlice from "./reducers/categorias";
 import itensSlice from './reducers/itens'
+import carrinhoSlice from './reducers/carrinho'
 
 const store = configureStore({
     reducer:{
         categorias: categoriaSlice,
-        itens: itensSlice
+        itens: itensSlice,
+        carrinho: carrinhoSlice,
     }
 })
 
