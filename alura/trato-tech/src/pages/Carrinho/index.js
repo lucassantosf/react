06991068cff7b643252e6b7d731d412a5916f1c7
Lucas <lucas46em@gmail.com>
2023-07-04 15:52:56 -0300

    init form anuncie

--- alura/trato-tech/src/pages/Carrinho/index.js
+++ alura/trato-tech/src/pages/Carrinho/index.js
@@ -3,6 +3,7 @@ import styles from './Carrinho.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import Item from 'components/Item';
 import { resetarCarrinho } from 'store/reducers/carrinho';
+import Button from 'components/Button';
 
 export default function Carrinho() {
   const dispatch = useDispatch()
@@ -42,12 +43,9 @@ export default function Carrinho() {
           <span>
             Subtotal: <strong> R$ {total.toFixed(2)} </strong>
           </span>
-          <button 
-            className={styles.finalizar}
-            onClick={()=>dispatch(resetarCarrinho())}
-          >
+          <Button onClick={()=>dispatch(resetarCarrinho())}>
             Finalizar Compra
-          </button>
+          </Button>
         </div>
       </div>
     </div>
